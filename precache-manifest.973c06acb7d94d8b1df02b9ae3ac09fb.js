248f288b643547efbdd6695d0a58b4f7b9bb8ff6
AVAVT <itsavavt@gmail.com>
2019-10-24 23:36:28 +0700

    Updates

---
+++ precache-manifest.973c06acb7d94d8b1df02b9ae3ac09fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aec5d69ea1a8d55351dbcaf963cc1b7",
+    "url": "/rell-game/index.html"
+  },
+  {
+    "revision": "629315c5bd938eaf1d3f",
+    "url": "/rell-game/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "be22c1b713c64ad0ae04",
+    "url": "/rell-game/static/css/main.f7bc442c.chunk.css"
+  },
+  {
+    "revision": "629315c5bd938eaf1d3f",
+    "url": "/rell-game/static/js/2.3177d957.chunk.js"
+  },
+  {
+    "revision": "be22c1b713c64ad0ae04",
+    "url": "/rell-game/static/js/main.43366d84.chunk.js"
+  },
+  {
+    "revision": "9244f50151b3a77f73d5",
+    "url": "/rell-game/static/js/runtime-main.caeb6e14.js"
+  }
+]);
