4c25e6ff0ca8dc22a18df0df7c3a7f88cb2ae7e9
AVAVT <itsavavt@gmail.com>
2019-11-01 19:55:38 +0700

    Updates

---
+++ precache-manifest.2f720614344743cdd63b456bdab97d88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bc7f0609ae230185944c37fda48dd5",
+    "url": "/rell-game/index.html"
+  },
+  {
+    "revision": "29bbc673b24a0e65691b",
+    "url": "/rell-game/static/css/main.d42950d7.chunk.css"
+  },
+  {
+    "revision": "4a39985ba55362924346",
+    "url": "/rell-game/static/js/2.63cb66b2.chunk.js"
+  },
+  {
+    "revision": "29bbc673b24a0e65691b",
+    "url": "/rell-game/static/js/main.1ab74e41.chunk.js"
+  },
+  {
+    "revision": "9244f50151b3a77f73d5",
+    "url": "/rell-game/static/js/runtime-main.caeb6e14.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rell-game/precache-manifest.973c06acb7d94d8b1df02b9ae3ac09fb.js"
+  "/rell-game/precache-manifest.2f720614344743cdd63b456bdab97d88.js"
 );
 
 self.addEventListener('message', (event) => {
